0995c531350ada23ba7a5f282800ec2e0a1530cd
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-05-11 10:51:00 +0800

    if then statement

--- src/main/java/sample/basic/Hello.java
+++ src/main/java/sample/basic/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Tim");
-        System.out.print("Hello World");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
