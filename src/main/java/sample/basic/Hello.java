a26196092f70387d89a01aedf34276fc19456ff7
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-05-12 14:51:29 +0800

    logical or operator

--- src/main/java/sample/basic/Hello.java
+++ src/main/java/sample/basic/Hello.java
@@ -16,9 +16,34 @@ public class Hello {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100) {
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less than 100");
         }
+
+        if ((topScore > 90) || (secondTopScore < 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is an true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+
+        System.out.println(s);
     }
 }
