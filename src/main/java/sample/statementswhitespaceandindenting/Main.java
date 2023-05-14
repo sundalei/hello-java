1a1aa51611db9200a6b3ab22476b92e3a151a53b
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-05-14 12:33:50 +0800

    statements, whitespace and indentation

---
+++ src/main/java/sample/statementswhitespaceandindenting/Main.java
@@ -0,0 +1,25 @@
+package sample.statementswhitespaceandindenting;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        anotherVariable--;
+        System.out.println("myVariable = " + myVariable);
+
+        if (myVariable == 0) {
+            System.out.println("It's now zero");
+        }
+    }
+}
