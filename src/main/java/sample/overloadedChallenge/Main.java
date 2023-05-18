6a494cd8117e728145513f06743338967d929fc4
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-05-18 11:36:50 +0800

    overload challenge 1

---
+++ src/main/java/sample/overloadedChallenge/Main.java
@@ -0,0 +1,20 @@
+package sample.overloadedChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("5ft, 8in = " + convertToCentimeters(5, 8) + "cm");
+        System.out.println("68in = " + convertToCentimeters(68) + "cm");
+    }
+
+    public static double convertToCentimeters(int inches) {
+
+        return inches * 2.54;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+
+        return convertToCentimeters((feet * 12) + inches);
+    }
+}
