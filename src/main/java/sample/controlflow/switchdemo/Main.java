e0cc54a60dfd09d169dfda7092afbef48f9d60e8
sundalei <sundalei1988@gmail.com>
2023-05-19 12:34:21 +0800

    if else if condition

---
+++ src/main/java/sample/controlflow/switchdemo/Main.java
@@ -0,0 +1,16 @@
+package sample.controlflow.switchdemo;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int value = 3;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+    }
+}
